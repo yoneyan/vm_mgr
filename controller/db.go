42bf99d501107044ebc39a06cfa377797ddcaa4d
yoneyan <github@yoneyan.net>
2020-01-05 16:10:24 +0900

    controller node db create test

---
+++ controller/db.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+const db_name = "./main.sql"
+
+func initdb() bool {
+
+	value := true
+
+	db, err := sql.Open("sqlite3", db_name)
+	if err != nil {
+		log.Fatalln(err)
+		fmt.Println("SQL open error")
+		panic(err)
+		value = false
+	}
+
+	_, err = db.Exec(
+		`CREATE TABLE IF NOT EXISTS "BOOKS" ("ID" INTEGER PRIMARY KEY, "TITLE" VARCHAR(255))`,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	/*
+		//create controller data table
+		_, err = db.Exec(`CREATE TABLE IF NOT EXISTS "controller"{"id" INTEGER PRIMARY KEY,"name" varchar,"ip" varchar,"port" integer)`,)
+		if err != nil{
+			log.Fatalln(err)
+			fmt.Println("failed create controller data table")
+			panic(err)
+			value = false
+		}
+
+		//create node data table
+		_, err = db.Exec(`CREATE TABLE IF NOT EXISTS "node"("id" INTEGER PRIMARY KEY,"name" varchar,"ip" varchar,"port" integer,"cpu" integer,"memory" integer,"storage" integer)`,)
+		if err != nil{
+			log.Fatalln(err)
+			panic(err)
+			value = false
+		}
+
+		//create vm data table
+		_, err = db.Exec(`CREATE TABLE IF NOT EXISTS "node"("id" INTEGER PRIMARY KEY,"name" varchar,"vcpu" integer,"vmemory" integer,"vstorage" integer,"vnc" integer,)`,)
+		if err != nil{
+			log.Fatalln(err)
+			panic(err)
+			value = false
+		}
+
+	*/
+	return value
+}
--- controller/main.go
+++ controller/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	//"github.com/urfave/cli"
+	"gopkg.in/urfave/cli.v1"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	app.Name = "vm_mgr"
+	app.Usage = "v0.001"
+	app.Version = "0.001"
+	app.Commands = []cli.Command{
+		{
+			Name:    "add",
+			Aliases: []string{"a"},
+			Usage:   "node add",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Not implemented")
+				return nil
+			},
+		},
+		{
+			Name:    "test",
+			Aliases: []string{"t"},
+			Usage:   "test script",
+			Action: func(c *cli.Context) error {
+				fmt.Println(initdb())
+				return nil
+			},
+		},
+	}
+	app.Run(os.Args)
 }
