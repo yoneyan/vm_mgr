215e1e737742c314a575af3f4738a28810454c23
yoneyan <github@yoneyan.net>
2020-02-22 04:56:43 +0900

    modify proto-go

--- proto/proto-go/virtualmachine.pb.go
+++ proto/proto-go/virtualmachine.pb.go
@@ -314,7 +314,7 @@ var _ context.Context
 var _ grpc.ClientConnInterface
 
 // This is a compile-time assertion to ensure that this generated file
-// is compatible with the grpc package it is being compiled against.
+// is compatible with the data package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion6
 
 // VMClient is the client API for VM service.
