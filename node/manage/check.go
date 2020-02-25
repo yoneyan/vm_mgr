102bc589a35e66bb51e6e7fac316317a39639a93
yoneyan <github@yoneyan.net>
2020-02-25 17:29:23 +0900

    add check function

--- node/manage/check.go
+++ node/manage/check.go
@@ -1,9 +1,40 @@
 package manage
 
+import (
+	"fmt"
+	"github.com/yoneyan/vm_mgr/node/db"
+)
+
 func VMExistsName(name string) bool {
-	return false
+	_, err := db.VMDBGetVMID(name)
+	if err != nil {
+		return false
+	}
+	return true
 }
 
 func VMExistsID(id int) bool {
-	return false
+	_, err := db.VMDBGetData(id)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func VMExistsCheck(name string, id int) bool {
+	if VMExistsID(id) || VMExistsName(name) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func VMIDCheck(id int) bool {
+	if id < 0 {
+		fmt.Println("VMID Check NG.")
+		return false
+	} else {
+		fmt.Println("VMID Check OK.")
+		return true
+	}
 }
