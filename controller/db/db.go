681c9e5247bdc7d1c21f70bdfb5a9e95410555b2
yoneyan <github@yoneyan.net>
2020-02-29 16:17:55 +0900

    add db tool for group and user

--- controller/db/db.go
+++ controller/db/db.go
@@ -8,7 +8,7 @@ import (
 	"log"
 )
 
-const db_name = "./main.sql"
+const DBName = "./main.sql"
 
 type Node struct {
 	ID       int
@@ -21,14 +21,14 @@ type Node struct {
 	Status   int
 }
 
-type VmUser struct {
+type User struct {
 	ID   int
 	Name string
 	Pass string
 	Auth int
 }
 
-type VmGroup struct {
+type Group struct {
 	ID         int
 	Name       string
 	Admin      string
@@ -39,7 +39,7 @@ type VmGroup struct {
 }
 
 func connectdb() *sql.DB {
-	db, err := sql.Open("sqlite3", db_name)
+	db, err := sql.Open("sqlite3", DBName)
 	if err != nil {
 		log.Fatalln(err)
 		fmt.Println("SQL open error")
@@ -62,11 +62,11 @@ func createdb(database string) bool {
 	return true
 }
 
-func Initdb() bool {
+func InitDB() bool {
 	//Node data
 	createdb(`CREATE TABLE IF NOT EXISTS "node" ("id" INTEGER PRIMARY KEY, "hostname" VARCHAR(255), "ip" VARCHAR(255), "port" INT, "auth" INT,"maxcpu" INT "maxmem" INT "status" INT`)
 	//user data
-	createdb(`CREATE TABLE IF NOT EXISTS "user" ("id" INTEGER PRIMARY KEY, "name" VARCHAR(255), "pass" VARCHAR(255),"auth" INT`)
+	createdb(`CREATE TABLE IF NOT EXISTS "user" ("id" INTEGER PRIMARY KEY, "name" VARCHAR(255), "pass" VARCHAR(255)`)
 	//group data
 	createdb(`CREATE TABLE IF NOT EXISTS "group" ("id" INTEGER PRIMARY KEY, "name" VARCHAR(255),"user" VARCHAR(10000),"admin" VARCHAR(1000),"maxcpu" INT,"maxmem" INT,"maxstorage" INT`)
 	return true
@@ -152,12 +152,11 @@ func GetDBAllNode() []Node {
 		bg = append(bg, b)
 	}
 	return bg
-
 }
 
 //User
 
-func AddDBUser(data VmUser) bool {
+func AddDBUser(data User) bool {
 	db := connectdb()
 	addDb, err := db.Prepare(`INSERT INTO "user" ("name","pass") VALUES (?,?)`)
 	if err != nil {
@@ -184,18 +183,89 @@ func DeleteDBUser(name string) bool {
 	return true
 }
 
-func TestPassDBUser(name, pass string) bool {
+func PassAuthDBUser(name, pass string) bool {
 	db := connectdb()
 	var hash string
 	if err := db.QueryRow("SELECT pass FROM user WHERE name = ?", name).Scan(&hash); err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("User Auth Success")
 
 	return etc.Verifyhashdata(pass, hash)
 }
 
+func GetDBUserID(name string) (int, bool) {
+	db := connectdb()
+
+	rows, err := db.Query("SELECT * FROM user WHERE name = ?", name)
+	if err != nil {
+		fmt.Println(err)
+		return 0, false
+	}
+	defer rows.Close()
+
+	var b User
+	err = rows.Scan(b.ID, b.Name, b.Pass)
+
+	if err != nil {
+		fmt.Println(err)
+		return 0, false
+	}
+	return b.ID, true
+}
+
+func GetDBAllUser() []User {
+
+	db := *connectdb()
+
+	rows, err := db.Query("SELECT * FROM user")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer rows.Close()
+
+	var bg []User
+	for rows.Next() {
+		var b User
+		err := rows.Scan(b.ID, b.Name, b.Pass)
+		if err != nil {
+			log.Println(err)
+		}
+		bg = append(bg, b)
+	}
+	return bg
+}
+
+func ChangeDBUserName(id int, data string) bool {
+	db := connectdb()
+
+	dbdata := "UPDATE user SET user = ? WHERE id = ?"
+	_, err := db.Exec(dbdata, data, id)
+
+	if err != nil {
+		fmt.Println("Error: DBUpdate Error (User Name)")
+		return false
+	}
+
+	return true
+}
+
+func ChangeDBUserPassword(id int, data string) bool {
+	db := connectdb()
+
+	dbdata := "UPDATE user SET pass = ? WHERE id = ?"
+	_, err := db.Exec(dbdata, etc.Hashgenerate(data), id)
+
+	if err != nil {
+		fmt.Println("Error: DBUpdate Error (User Pass)")
+		return false
+	}
+
+	return true
+}
+
 //group
-func AddDBGroup(data VmGroup) bool {
+func AddDBGroup(data Group) bool {
 	db := connectdb()
 	addDb, err := db.Prepare(`INSERT INTO "group" ("name","user","admin","maxcpu","maxmem","maxstorage") VALUES (?,?,?,?,?,?)`)
 	if err != nil {
@@ -211,13 +281,115 @@ func AddDBGroup(data VmGroup) bool {
 	return true
 }
 
-func DeleteDBGroup(name string) bool {
+func DeleteDBGroup(id int) bool {
 	db := connectdb()
-	deleteDb := "DELETE FROM group WHERE name = ?"
-	_, err := db.Exec(deleteDb, name)
+	deletedb := "DELETE FROM group WHERE id = ?"
+	_, err := db.Exec(deletedb, id)
 	if err != nil {
 		log.Fatalln(err)
 		return false
 	}
+	return true
+}
+
+func GetDBAllGroup() []Group {
+	db := *connectdb()
+	rows, err := db.Query("SELECT * FROM group")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer rows.Close()
+
+	var bg []Group
+	for rows.Next() {
+		var b Group
+		err := rows.Scan(b.ID, b.Name, b.User, b.Admin, b.MaxCPU, b.MaxMem, b.MaxStorage)
+		if err != nil {
+			log.Println(err)
+		}
+		bg = append(bg, b)
+	}
+	return bg
+}
+
+func GetDBGroup(id int) (Group, bool) {
+	db := connectdb()
+	rows, err := db.Query("SELECT * FROM group WHERE id = ?", id)
+	if err != nil {
+		fmt.Println(err)
+		return Group{}, false
+	}
+	defer rows.Close()
+
+	var b Group
+	err = rows.Scan(b.ID, b.Name, b.User, b.Admin, b.MaxCPU, b.MaxMem, b.MaxStorage)
+
+	if err != nil {
+		fmt.Println(err)
+		return b, false
+	}
+	return b, true
+}
+
+func GetDBGroupID(name string) (int, bool) {
+	db := connectdb()
+
+	rows, err := db.Query("SELECT * FROM group WHERE name = ?", name)
+	if err != nil {
+		fmt.Println(err)
+		return 0, false
+	}
+	defer rows.Close()
+
+	var b Group
+	err = rows.Scan(b.ID, b.Name, b.User, b.Admin, b.MaxCPU, b.MaxMem, b.MaxStorage)
+
+	if err != nil {
+		fmt.Println(err)
+		return 0, false
+	}
+	return b.ID, true
+
+}
+
+func ChangeDBGroupName(id int, data string) bool {
+	db := connectdb()
+
+	dbdata := "UPDATE group SET name = ? WHERE id = ?"
+	_, err := db.Exec(dbdata, data, id)
+
+	if err != nil {
+		fmt.Println("Error: DBUpdate Error (Group Name)")
+		return false
+	}
+
+	return true
+}
+
+func ChangeDBGroupAdmin(id int, data string) bool {
+	db := connectdb()
+
+	dbdata := "UPDATE group SET admin = ? WHERE id = ?"
+	_, err := db.Exec(dbdata, data, id)
+
+	if err != nil {
+		fmt.Println("Error: DBUpdate Error (Group Admin)")
+		return false
+	}
+
+	return true
+}
+
+func ChangeDBGroupUser(id int, data string) bool {
+	db := connectdb()
+
+	dbdata := "UPDATE group SET user = ? WHERE id = ?"
+	_, err := db.Exec(dbdata, data, id)
+
+	if err != nil {
+		fmt.Println("Error: DBUpdate Error (Group User)")
+		return false
+	}
+
 	return true
 }
