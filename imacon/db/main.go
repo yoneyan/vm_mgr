a77b53ea1dfd5b50dc6918ef23e5906fb66a0c41
yoneyan <github@yoneyan.net>
2020-03-19 00:22:45 +0900

    add database system for imacon

--- imacon/db/main.go
+++ imacon/db/main.go
@@ -8,18 +8,20 @@ import (
 
 const DBPath = "./main.db"
 
-type Node struct {
+type Image struct {
 	ID        int
-	HostName  string
-	IP        string
-	Path      string
-	OnlyAdmin int
-	MaxCPU    int
-	MaxMem    int
+	FileName  string
+	Name      string
+	Tag       string
+	Type      int
+	Capacity  int
+	AddTime   int
+	Authority int
+	MinMem    int
 	Status    int
 }
 
-func conneectDB() *sql.DB {
+func connectDB() *sql.DB {
 	db, err := sql.Open("sqlite3", DBPath)
 	if err != nil {
 		fmt.Println("SQL open error")
@@ -32,7 +34,7 @@ func conneectDB() *sql.DB {
 }
 
 func createDB(database string) bool {
-	db := *conneectDB()
+	db := *connectDB()
 
 	_, err := db.Exec(database)
 	if err != nil {
@@ -43,7 +45,8 @@ func createDB(database string) bool {
 }
 
 func InitDB() bool {
-	//ImageData
-	createDB(`CREATE TABLE IF NOT EXISTS "imagedata" ("id" INTEGER PRIMARY KEY, "name" VARCHAR(500), "type" INT,"capacity" INT, "addtime" INT, "minmem" INT,"onlyadmin" INT)`)
+	//BaseImageData
+	createDB(`CREATE TABLE IF NOT EXISTS "image" ("id" INTEGER PRIMARY KEY, "filename" VARCHAR(500), "name" VARCHAR(500),"tag" VARCHAR(500),"type" INT,"capacity" INT, "addtime" INT, "minmem" INT,"authority" INT,"status" INT)`)
+
 	return true
 }
