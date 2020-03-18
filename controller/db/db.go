94476674f89e40f94b81f43f656785457536689d
yoneyan <github@yoneyan.net>
2020-03-19 04:43:26 +0900

    add imacon and progress database for controller

--- controller/db/db.go
+++ controller/db/db.go
@@ -19,6 +19,13 @@ type Node struct {
 	Status    int
 }
 
+type ImaCon struct {
+	ID       int
+	HostName string
+	IP       string
+	Status   int
+}
+
 type User struct {
 	ID   int
 	Name string
@@ -47,6 +54,13 @@ type Token struct {
 	Endtime   int
 }
 
+type Progress struct {
+	ID        int
+	VMName    string
+	UUID      string
+	StartTime int
+}
+
 func connectdb() *sql.DB {
 	db, err := sql.Open("sqlite3", DBPath)
 	if err != nil {
@@ -73,12 +87,15 @@ func createdb(database string) bool {
 func InitDB() bool {
 	//Node data
 	createdb(`CREATE TABLE IF NOT EXISTS "node" ("id" INTEGER PRIMARY KEY, "hostname" VARCHAR(255), "ip" VARCHAR(255), "path" VARCHAR(2000), "onlyadmin" INT,"maxcpu" INT ,"maxmem" INT, "status" INT)`)
+	//imacon data
+	createdb(`CREATE TABLE IF NOT EXISTS "imacon" ("id" INTEGER PRIMARY KEY, "hostname" VARCHAR(255), "ip" VARCHAR(255), "status" INT)`)
 	//user data
 	createdb(`CREATE TABLE IF NOT EXISTS "userdata" ("id" INTEGER PRIMARY KEY, "name" VARCHAR(255), "pass" VARCHAR(255))`)
 	//group data
 	createdb(`CREATE TABLE IF NOT EXISTS "groupdata" ("id" INTEGER PRIMARY KEY, "name" VARCHAR(255),"admin" VARCHAR(500),"user" VARCHAR(2000),"maxvm" INT,"maxcpu" INT,"maxmem" INT,"maxstorage" INT,"net" VARCHAR(255))`)
 	//token data
 	createdb(`CREATE TABLE IF NOT EXISTS "tokendata" ("id" INTEGER PRIMARY KEY, "token" VARCHAR(1000), "userid" INT,"groupid" INT,"begintime" INT,"endtime" INT)`)
-
+	//progress data
+	createdb(`CREATE TABLE IF NOT EXISTS "progress" ("id" INTEGER PRIMARY KEY, "vmname" VARCHAR(255), "uuid" VARCHAR(255), "starttime" INT)`)
 	return true
 }
---
+++ controller/db/progress.go
@@ -0,0 +1,78 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+func AddDBProgress(data Progress) bool {
+	fmt.Println(data)
+	db := *connectdb()
+	addDb, err := db.Prepare(`INSERT INTO "progress" ("vmname","uuid","starttime") VALUES (?,?,?)`)
+	if err != nil {
+		fmt.Println("DBError!!")
+		return false
+	}
+
+	if _, err := addDb.Exec(data.VMName, data.UUID, data.StartTime); err != nil {
+		fmt.Println("Add Error!!")
+		return false
+	}
+
+	return true
+}
+
+func RemoveDBProgress(id int) bool {
+	db := *connectdb()
+	deleteDb := "DELETE FROM progress WHERE id = ?"
+	_, err := db.Exec(deleteDb, id)
+	if err != nil {
+		fmt.Println("Delete Failed!!")
+		return false
+	}
+	return true
+}
+
+func GetDBProgressVMName(vmname string) (Progress, bool) {
+	db := *connectdb()
+
+	rows := db.QueryRow("SELECT * FROM progress WHERE vmname = ?", vmname)
+
+	var b Progress
+	err := rows.Scan(&b.ID, &b.VMName, &b.UUID, &b.StartTime)
+
+	switch {
+	case err == sql.ErrNoRows:
+		fmt.Printf("Not found")
+		return b, false
+	case err != nil:
+		fmt.Println(err)
+		fmt.Println("Error: DBError")
+		return b, false
+	default:
+		return b, true
+	}
+}
+
+func GetDBAllProgress() []Progress {
+
+	db := *connectdb()
+
+	rows, err := db.Query("SELECT * FROM progress")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer rows.Close()
+
+	var bg []Progress
+	for rows.Next() {
+		var b Progress
+		err := rows.Scan(&b.ID, &b.VMName, &b.UUID, &b.StartTime)
+		if err != nil {
+			log.Println(err)
+		}
+		bg = append(bg, b)
+	}
+	return bg
+}
